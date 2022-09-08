c4070beafb0fa1c458ff097b860744a8466b8d91
Stephen Olude <oludefstephen@gmail.com>
2022-09-08 16:57:58 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 
 #include<stdio.h>
+
 /**
  * main - Entry point
- *Description: using the main function
- this program prints Programming is like building a multilingual puzzle
- * return: 0
+ * Description: using the main function
+ * this program prints Programming is like building a multilingual puzzle
+ * Return: 0
  */
 int main(void)
 {

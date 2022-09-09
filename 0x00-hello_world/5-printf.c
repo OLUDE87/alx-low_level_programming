bb838e04b52f1c5514b6822b5795d90da31dba76
Stephen Olude <oludefstephen@gmail.com>
2022-09-08 17:14:22 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * Main - entry point
+ * Main - print the string in the put function
+ *
  * Description: using the main function
  * This program prints with proper grammar, but the outcome is a piece of art,
- * Return 0
+ * Return: 0
  */
 int main(void)
 {

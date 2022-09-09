1dbfb9a51b242a88d46b51df6ce37fd1c5b494ba
Stephen Olude <oludefstephen@gmail.com>
2022-09-09 16:23:26 -0700

    Print last digits

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,12 +21,13 @@ int main(void)
 	if (n > 5)
 	{
 		printf("%d and is greater than 5\n", n);
-	}else if (n == 0)
+	} else if (n == 0)
 	{
 		printf("%d and is 0\n", n);
-	}else
+	} else
 	{
 		printf("%d is less than 6 and not 0\n", n);
 	}
 	return (0);
 }
+

648aacdae762aaa70c90ff6b2d895c454438225a
Stephen Olude <oludefstephen@gmail.com>
2022-09-08 18:32:11 -0700

    quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints and that piece of art is useful
+ * return: always 1 (success)
+ */
+
+int main(void)
+
+{
+
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n" );
+	return (1);
+
+
+}

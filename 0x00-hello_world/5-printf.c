48bfe954facd763dc62ace772e74544019cdeaad
Stephen Olude <oludefstephen@gmail.com>
2022-09-08 17:18:11 -0700

    Hello, puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * Main - print the string in the put function
- *
- * Description: using the main function
- * This program prints with proper grammar, but the outcome is a piece of art,
- * Return: 0
- */
+* main - print the string in the put function
+*
+* Description: using the main function
+* This program prints programming is like building a multiligual puzzle
+* Return: 0
+*/
 int main(void)
 {
 

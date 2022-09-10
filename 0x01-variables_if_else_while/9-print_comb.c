069cdc53814c92be65f151c418da8f7c6ecdc195
Stephen Olude <oludefstephen@gmail.com>
2022-09-09 19:31:31 -0700

    possible commbination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,10 +13,10 @@ int main(void)
 
 	int i;
 
-	for (i = 20; i < 40; i++)
+	for (i = 10; i < 20; i++)
 	{
-		putchar((i % 20) + '0');
-	if (i != 39)
+		putchar((i % 10) + '0');
+	if (i != 19)
 	{
 		putchar(',');
 		putchar(' ');

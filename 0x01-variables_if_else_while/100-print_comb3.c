ea47493aceb7fb4273c60f7b6f7cfa9a8eb0b17c
Stephen Olude <oludefstephen@gmail.com>
2022-09-09 20:08:42 -0700

    commbination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,7 +13,6 @@ int main(void)
 
 	int i;
 	int j;
-
 	
 	for (i = 10 ; i <= 19 ; i++)
 	{
@@ -23,16 +22,14 @@ int main(void)
 			{
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
-       				if (i != 18 || j != 19)
-      				 {
-			       		putchar(',');
-	   			        putchar(' ');
+				if (i != 18 || j != 19)
+				{
+					putchar(',');
+					putchar(' ');
 		       		}	       
 			}
 		}
 	}
 	putchar('\n');
 	return (0);
-
-	
 }

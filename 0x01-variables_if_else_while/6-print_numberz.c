32f0a80529a84cceea827e268db43c501d552e78
Stephen Olude <oludefstephen@gmail.com>
2022-09-09 18:33:42 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print numberz
+ *
+ * Description: using the main function
+ * this program prints programming print numberz
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	int i = 0;
+
+	for (; i < 10 ; i++)
+		putshar('0' + a);
+	putchar('\n');
+	return (0);
+}

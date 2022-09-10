0df1d58f2b42f89db3f1ae9f491b5ce1e9927d4e
Stephen Olude <oludefstephen@gmail.com>
2022-09-09 17:54:55 -0700

    print alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,8 +12,14 @@ int main(void)
 {
 
 	char a;
-	for (a < 'e' ; a > 'e' ; a < 'q' ; a > 'q' ; a <= 'z' ; a++)
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+	if (a == 'q' || a == 'e')
+		{
+			continue;
+		}
 		putchar(a);
+	}
 	putchar('\n');
 	return (0);
 }

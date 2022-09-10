d754690d0f0731812963b6c1693abd32f2a37f70
Stephen Olude <oludefstephen@gmail.com>
2022-09-09 19:16:58 -0700

    posible combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print cominbation of single digit
+ *
+ * Description: using the main function
+ * this program prints programming print combination of single digit
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	int c;
+
+	for ( c = 48; c <= 57; c++)
+		putchar(c);
+	if ( c != 57)
+		putchar(',');
+		putchar(' ');
+	putchar('\n');
+	return (0);
+}

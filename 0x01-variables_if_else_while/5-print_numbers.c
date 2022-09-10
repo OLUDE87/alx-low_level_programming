66a6eaae6cfb4e0010f544c71fa9e965379aa917
Stephen Olude <oludefstephen@gmail.com>
2022-09-09 18:18:32 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Description: using the main function
+ * the program prints programming print numbers
+ * Return 0
+ */
+
+int main(voi)
+{
+
+	int a = 0;
+
+	for (; a < 10 ; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}

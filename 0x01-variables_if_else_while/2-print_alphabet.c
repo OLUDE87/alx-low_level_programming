02a08ca4283fff4565f08eab24299d4d78039ca7
Stephen Olude <oludefstephen@gmail.com>
2022-09-09 17:00:42 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main print alphabet
+ *
+ * Description: using the main function
+ * this program prints programming the print alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	putchar("\n");
+	return (0);
+}

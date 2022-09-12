bc075bfd9158175499fe5681483bcc8016df131c
Stephen Olude <oludefstephen@gmail.com>
2022-09-12 08:47:24 -0700

    combination of two two-digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,10 @@ int main(void)
 	{
 		for (j = 1 ; j < 99 ; j++)
 		{
+			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 
 			if (i == 98 || j == 99)

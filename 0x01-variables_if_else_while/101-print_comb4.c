2d607d7e8b99c892c18007591379fe8f2419eaf8
Stephen Olude <oludefstephen@gmail.com>
2022-09-12 07:10:43 -0700

    combination of 3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -24,8 +24,9 @@ int main(void)
 				putchar(j + '0');
 				putchar(k + '0');
 
-				if (i == 7 || j == 8 || k == 9)
-					
+				if (i == 7 && j == 8 && k == 9)
+					continue;
+
 				putchar(',');
 				putchar(' ');
 

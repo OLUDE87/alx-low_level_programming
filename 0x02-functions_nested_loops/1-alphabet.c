ca5aa9351a3554942a772f3cd3378ae65fa15531
Stephen Olude <oludefstephen@gmail.com>
2022-09-13 06:31:57 -0700

    print_alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - print print the alphabet in lower case
+ * main - print_alphabet
  *
  * Description: using the main function
  * this program print programing print the alphabet in lowercase
@@ -10,12 +10,12 @@
  */
 void print_alphabet(void)
 {
-
 	char i;
 	
 	for (i = 'a' ; i <= 'z' ; i++)
+	{
 		_putchar(i);
-
+	}
 	_putchar('\n');
-
+	
 }

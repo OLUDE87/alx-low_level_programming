407c61591308b463ecc568a8f7c600ae3dba7aab
Stephen Olude <oludefstephen@gmail.com>
2022-09-13 06:28:26 -0700

    print alhabeth in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print print the alphabet in lower case
+ *
+ * Description: using the main function
+ * this program print programing print the alphabet in lowercase
+ * Return: 0
+ *
+ */
+void print_alphabet(void)
+{
+
+	char i;
+	
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+
+	_putchar('\n');
+
+}

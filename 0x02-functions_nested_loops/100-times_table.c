23cf7728b5d742fe8013aba3ec281a47c070d918
Stephen Olude <oludefstephen@gmail.com>
2022-09-13 14:00:37 -0700

    times tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,9 +31,9 @@ void print_times_table(int n)
 					_putchar((k / 10) % 10 + '0');
 				}
 				else if (k <= 99 && k >= 10)
-					{
-						_putchar((k / 10) + '0');
-					}
+				{
+					_putchar((k / 10) + '0');
+				}
 					_putchar((k % 10) + '0');
 				}
 				_putchar('\n');
